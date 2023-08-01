02f24c4cf4f34cd708e01d631173886cac25c6be
apuravgupta86 <apuravgupta86@gmail.com>
2023-08-01 11:28:44 +0530

    Some changes

--- src/controllers/blog_info_cont.js
+++ src/controllers/blog_info_cont.js
@@ -31,8 +31,6 @@ const add_info = async(req,res)=>{
         return res.send(formerror)
     }
 
-
-    
     const getData = {
         user_id:req.body.user_id,
         blog_name:req.body.blog_name,
